9fc3a0ca8c3d9deaf3df7846f3fa63ecc0d7e0f2
nyankosoft <sgt.nyanko@gmail.com>
2018-10-20 20:20:18 +0900

    Trying out TabLayout and TabItem.

--- app/src/main/java/space/nyanko/nyankoapplication/MainActivity.java
+++ app/src/main/java/space/nyanko/nyankoapplication/MainActivity.java
@@ -34,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
     }
 
     @Override
