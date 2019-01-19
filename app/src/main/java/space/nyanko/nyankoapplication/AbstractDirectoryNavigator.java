b01baf2fe4f693578704adc09cee7d027cbc26d9
nyankosoft <sgt.nyanko@gmail.com>
2019-01-19 11:08:50 +0900

    Added comments.

--- app/src/main/java/space/nyanko/nyankoapplication/AbstractDirectoryNavigator.java
+++ app/src/main/java/space/nyanko/nyankoapplication/AbstractDirectoryNavigator.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 
 public abstract class AbstractDirectoryNavigator {
 
+    /**
+     * @brief Returns a list containing 2 types of objects
+     * 1. Media file(s) Horizon is capable of playing
+     * 2. Directory/directories
+     *
+     * Directories might or might not have media files in them.
+     *
+     * @return
+     */
     abstract ArrayList<File> getCurrentDirectoryEntries();
 
     abstract String getCurrentDirectoryName();
