c8f48470bdbd202ea95d3ef4d2370fbc13f30b04
Takashi Kumagai <nyankophile@gmail.com>
2019-01-31 02:38:26 +0900

    Changed FAB. Draws the left-pointing triangle as a bitmp instead of a character.

--- app/src/main/java/space/nyanko/nyankoapplication/MainActivity.java
+++ app/src/main/java/space/nyanko/nyankoapplication/MainActivity.java
@@ -26,6 +26,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -739,6 +743,9 @@ public class MainActivity extends AppCompatActivity {
                 //}
             }
         });
+
+        fab.setImageBitmap(textAsBitmap("▶", 40, Color.WHITE));
+
         fab.setVisibility(View.GONE);
     }
 
@@ -839,6 +846,22 @@ public class MainActivity extends AppCompatActivity {
         // Show the playing track info and control (pause/resume)
         View ptc = findViewById(R.id.playing_track_control);
         ptc.setVisibility(View.GONE);
+    }
+
+    //method to convert your text to image
+    public static Bitmap textAsBitmap(String text, float textSize, int textColor) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText(text) + 0.0f); // round
+        int height = (int) (baseline + paint.descent() + 0.0f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(text, 0, baseline, paint);
+        return image;
     }
 
     /**
