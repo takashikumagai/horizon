a8fdb3d07fc18a66824dfcb86925af76e847a9a5
Takashi Kumagai <nyankophile@gmail.com>
2019-02-03 18:56:34 +0900

    Updated a script.

--- start_logcat.sh
+++ start_logcat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-adb logcat MainActivity:D BackgroundAudioService:D mySessionTag:D RecyclerViewAdapter:V DirectoryNavigation:D StorageSelector:D DirectoryNavigator:D Playback:D HorizonUtils:D *:S
+adb logcat MainActivity:D BackgroundAudioService:D mySessionTag:D RecyclerViewAdapter:V DirectoryNavigation:D StorageSelector:D DirectoryNavigator:D Playback:D HorizonUtils:D LockScreenMediaControl:D *:S
