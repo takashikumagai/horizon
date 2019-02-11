be2c716fefba8b096d0bc216e9c54803e5ec1aa2
Takashi Kumagai <nyankophile@gmail.com>
2019-02-11 23:25:59 +0900

    Updated the log script. It now prints AndroidRuntime errors such as exceptions.

--- start_logcat.sh
+++ start_logcat.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-adb logcat MainActivity:D BackgroundAudioService:D mySessionTag:D RecyclerViewAdapter:V DirectoryNavigation:D StorageSelector:D DirectoryNavigator:D Playback:D HorizonUtils:D LockScreenMediaControl:D *:S
+adb logcat \
+MainActivity:D \
+BackgroundAudioService:D \
+mySessionTag:D \
+RecyclerViewAdapter:D \
+DirectoryNavigation:D \
+StorageSelector:D \
+DirectoryNavigator:D \
+Playback:D \
+HorizonUtils:D \
+LockScreenMediaControl:D \
+AndroidRuntime:E \
+*:S
