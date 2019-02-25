2a8aacd14c99368cd94b2c23c8429842cd88d4ef
Takashi Kumagai <nyankophile@gmail.com>
2019-02-25 20:47:24 +0900

    Checks the result code returned by the Gradle build.

--- build_and_push_apk_to_device.sh
+++ build_and_push_apk_to_device.sh
@@ -10,5 +10,6 @@ if [ $gradle_return_code -eq 0 ]
 then
   ./copy_apks_to_my_device.sh
 else
-  echo Build error(s). Check the error message.
+  echo $gradle_return_code
+  echo "Build error(s). Check the error message."
 fi
